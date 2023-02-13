bcd16b53b31c2e871fa56e2aeedd65cb21729e5f
sullaspqr <sulla.spqr78@gmail.com>
2023-02-13 08:15:59 +0100

    Add project files.

---
+++ Models/Végsőpont.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ertekeloRendszer.Models;
+
+public partial class Végsőpont
+{
+    public string Nev { get; set; } = null!;
+
+    public decimal? VégsőPont1 { get; set; }
+}
